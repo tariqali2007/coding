7646b189dcc347de5b24d618acb7057a8c2758f9
tariqalimahar <tariqalimahar2007@gmail.com>
2024-05-23 12:19:24 +0500

    class3

---
+++ class3.js
@@ -0,0 +1,73 @@
+// let ice = +prompt("inter your number");
+
+// if(ice === 100){
+//     console.log("ice-Cream is corenetto");
+// }else if(ice === 500){
+//     console.log(".ice-Cream is corenetto family pack 1.5 LTR");
+// }else if(ice === 1000){
+//     console.log("ice-Cream is corenetto family pack 2.5 LTR");
+// }else if(ice === 5000){
+//     console.log("ice-Cream is corenetto family pack 4.5 LTR");
+// }
+// else if(ice === 10000){
+//     console.log("ice-Cream is corenetto family pack 10 LTR");
+// }
+// else{
+//     console.log("your ice-cream is random ");
+// }
+
+// switch (ice) {
+//     case 100:
+//         console.log("ice-Cream is corenetto");
+//         break;
+//     case 500:
+//         console.log(".ice-Cream is corenetto family pack 1.5 LTR");
+//         break;
+//     case 1000:
+//         console.log("ice-Cream is corenetto family pack 2.5 LTR");
+//         break;
+//     default:
+//         console.log("your ice-cream is random ");
+//         break;
+
+// }
+
+// let userNum = prompt("Even number ");
+// let compNum = Math.round(Math.random() * 10);
+
+// if(compNum % 2 === 0  && userNum === "even"){
+//     console.log(" you win ! " + compNum);
+// }
+// else if(compNum % 2 !== 0 && userNum === "odd"){
+//     console.log(" you win ! " + compNum);
+// }else{
+//     console.log("you loss ! " + compNum);
+// }
+// initialistion
+///condition 
+///increment
+// for(let i = 0; i <= 100 ; i++){
+//     console.log("Hello world " + i );
+//     console.log(  i +  " Tariq " );
+// }
+
+// for(let i = 1; i <= 100 ; i++){
+//     console.log("Hello world " + i)
+// }
+// for(let i = 1; i <= 20 ; i++){
+//     console.log( " 2 X " + i +  " = " + 2 *i)
+// }
+// let table = 11;
+// for(let i = 1; i <= 10 ; i++){
+//     console.log(table +  " X " + i +  " = " + table *i)
+//     console.log(Math.round(Math.random() * 10));
+//     console.log("helloworld ");
+
+// }
+
+// let name = " Abdullah "
+// let last_name = "kham";
+// console.log(name + last_name); 
+//  let num = 55;   // number
+//  let num2 = '55'; // string 
+//  console.log(num + num2);
